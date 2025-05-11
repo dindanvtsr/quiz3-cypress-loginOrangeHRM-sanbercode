4d852f0c599cfcbcefe8a8bc78b1045daa06740b
dindanvtsr <dindaaisyah58@gmail.com>
2025-05-11 12:35:25 +0700

    commit 1.0

---
+++ quiz3-loginOrangeHRM/cypress/e2e/loginOrangeHRM.cy.js
@@ -0,0 +1,76 @@
+describe('Login', () => {
+    beforeEach(() => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    })
+
+    it('TCL_001', () => {
+        cy.xpath('//input[@placeholder="Username"]').should('be.visible').clear().type('Admin')
+        cy.xpath('//input[@placeholder="Username"]').should('have.value', 'Admin')
+
+        cy.xpath('//input[@placeholder="Password"]').should('be.visible').clear().type('admin123')
+        cy.xpath('//input[@placeholder="Password"]').should('have.value', 'admin123')
+
+        cy.get('button[type="submit"]').should('be.visible').click()
+        cy.url().should('include', '/dashboard')
+    })
+    it('TCL_002', () => {
+        cy.xpath('//input[@placeholder="Username"]').should('be.visible').clear().type('Admin')
+        cy.xpath('//input[@placeholder="Username"]').should('have.value', 'Admin')
+        
+        cy.xpath('//input[@placeholder="Password"]').should('be.visible').clear().type('admin')
+        cy.xpath('//input[@placeholder="Password"]').should('have.value', 'admin')
+
+        cy.get('button[type="submit"]').should('be.visible').click()
+        cy.xpath('//p[contains(@class, "oxd-alert-content-text")]').should('contain', 'Invalid credentials')
+    })
+    it('TCL_003', () => {
+        cy.xpath('//input[@placeholder="Username"]').should('be.visible').clear().type('Adminnn')
+        cy.xpath('//input[@placeholder="Username"]').should('have.value', 'Adminnn')
+
+        cy.xpath('//input[@placeholder="Password"]').should('be.visible').clear().type('admin123')
+        cy.xpath('//input[@placeholder="Password"]').should('have.value', 'admin123')
+
+        cy.get('button[type="submit"]').should('be.visible').click()
+        cy.xpath('//p[contains(@class, "oxd-alert-content-text")]').should('contain', 'Invalid credentials')
+    })
+    it('TCL_004', () => {
+        cy.xpath('//input[@placeholder="Username"]').should('be.visible').clear()
+        cy.xpath('//input[@placeholder="Username"]').should('have.value', '')
+
+        cy.xpath('//input[@placeholder="Password"]').should('be.visible').clear()
+        cy.xpath('//input[@placeholder="Password"]').should('have.value', '')
+
+        cy.get('button[type="submit"]').should('be.visible').click()
+        cy.xpath('//span[contains(@class, "oxd-input-field-error-message")]').should('contain', 'Required')
+    })
+    it('TCL_005', () => {
+        cy.xpath('//input[@placeholder="Username"]').should('be.visible').clear().type('coba')
+        cy.xpath('//input[@placeholder="Username"]').should('have.value', 'coba')
+
+        cy.xpath('//input[@placeholder="Password"]').should('be.visible').clear().type('coba123')
+        cy.xpath('//input[@placeholder="Password"]').should('have.value', 'coba123')
+
+        cy.get('button[type="submit"]').should('be.visible').click()
+        cy.xpath('//p[contains(@class, "oxd-alert-content-text")]').should('contain', 'Invalid credentials')
+    })
+    it('TCL_006', () => {
+        cy.xpath('//input[@placeholder="Username"]').should('be.visible').clear()
+        cy.xpath('//input[@placeholder="Username"]').should('have.value', '')
+
+        cy.xpath('//input[@placeholder="Password"]').should('be.visible').clear().type('admin123')
+        cy.xpath('//input[@placeholder="Password"]').should('have.value', 'admin123')
+
+        cy.get('button[type="submit"]').should('be.visible').click()
+        cy.xpath('//span[contains(@class, "oxd-input-field-error-message")]').should('contain', 'Required')
+    })
+    it('TCL_007', () => {
+        cy.xpath('//input[@placeholder="Username"]').should('be.visible').clear().type('Admin')
+        cy.xpath('//input[@placeholder="Username"]').should('have.value', 'Admin')
+
+        cy.xpath('//input[@placeholder="Password"]').should('be.visible').clear()
+        cy.xpath('//input[@placeholder="Password"]').should('have.value', '')
+
+        cy.get('button[type="submit"]').should('be.visible').click()
+        cy.xpath('//span[contains(@class, "oxd-input-field-error-message")]').should('contain', 'Required')
+    })
+})
